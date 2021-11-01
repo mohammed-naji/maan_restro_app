90908ec08c5bf03480419467f70279cad1cf5262
Mohammed <m.naji1512@gmail.com>
2021-11-01 21:27:37 +0200

    dashboard design

---
+++ app/Http/Middleware/CheckUserType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckUserType
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth()->user()->type == 'customer') {
+            Auth::logout(Auth::user());
+            return redirect('/');
+        }
+        return $next($request);
+    }
+}
